eebd32ed09fc8aef5b85e8ef700af4d5009cfe7c
subramanivasu <73416606+subramanivasu@users.noreply.github.com>
2021-09-26 22:35:11 +0530

    Add files via upload

---
+++ day16/test1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+    float number1, number2, sum, average; //Declaring Variables
+    cin >> number1; // Read Numbers 
+    cin >> number2; 
+    sum = number1 + number2;
+    average = sum / 2;
+    cout << "Sum = " << sum << "\n";
+    cout << "average = " << average << "\n";
+    cout << "Hehe \nDone!!!!!!!!!!!!!!!!!!!!" << std::endl;
+    return 0;
+} 
+
+
